61ef7248576ccccbaff43a199ae8b1e2866f68bc
krishbin <krishbinp@outlook.com>
2020-11-12 13:28:32 +0545

    added some config

--- .config/configure.sh
+++ .config/configure.sh
@@ -14,8 +14,14 @@ fi
 
 config config --local status.showUntrackedFiles no
 
+config submodule init
+
+config submodule update
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install autojump bat cmake dark-mode exa fd fzf gh gitui glfw glm grep llvm lua luajit luarocks ncdu neovim nettle node openssl@1.1 pkg-config python@3.8 python@3.9 ripgrep texlab tldr tmux tree tree-sitter
+
+brew cask install alfred appcleaner dash discord dropbox eul google-chrome hammerspoon iina iterm2 keka macdown macvim notion skim transmission
 
 source ~/.zshrc
